389f07c038006f8ca3b5524ba33d3f6f6e60a1a0
ynkyo <y-nishio@kyo-co.jp>
2025-05-18 00:12:57 +0900

    残りの修正

---
+++ Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = () => (
+  <View style={styles.header}>
+    <Text style={styles.headerTitle}>Today</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    marginBottom: 32,
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#3F414E',
+  },
+});
+
+export default Header; 
